3dda889e1b5b454209dfc1b936fecbcd3a7b7fed
Mattyy <89354292+NMattyy@users.noreply.github.com>
2026-03-06 18:08:21 +0100

    translation.h / Release 1.1.0

---
+++ BetterWinver/translation.h
@@ -0,0 +1,69 @@
+#ifndef TRANSLATION_H
+#define TRANSLATION_H
+
+#include "infoGet.h"
+
+string Language = currentLanguage();
+//0409 English_US
+//0410 Italian
+
+string string_1(){
+    if (Language == "0409"){
+        return "Right now BetterWinver only supports Windows 8 and newer (build 9200 and newer)";
+    } else if (Language == "0410") {
+        return "BetterWinver attualmente supporta solo Windows 8 e successivi (build 9200 e successive)";
+    } else {
+    return "Right now BetterWinver only supports Windows 8 and newer (build 9200 and newer)";
+    }
+}
+
+string string_2(){
+    if (Language == "0409"){
+        return "Warning: this version of Windows is not compatible";
+    } else if (Language == "0410") {
+        return "Attenzione: versione di Windows non compatibile";
+    } else {
+    return "Warning: this version of Windows is not compatible";
+    }
+}
+
+string string_3(){
+    if (Language == "0409"){
+        return "About Windows";
+    } else if (Language == "0410") {
+        return "Informazioni su Windows";
+    } else {
+    return "About Windows";
+    }
+}
+
+string string_4(){
+    if (Language == "0409"){
+        return string("Microsoft ") + OSGet() + " " + commercialVersionGet() + "\n"
+                    "NT Version " + ntGet() + " (OS build " + buildGet() + ")\n" + 
+                    (char)169 + " Microsoft Corporation. All rights reserved.\n\n" + 
+                    "The " + OSGet() + " operating system and its user interface are protected by trademark and other pending or existing intellectual property rights in the United States and other countries/regions";
+    } else if (Language == "0410") {
+        return string("Microsoft ") + OSGet() + " " + commercialVersionGet() + "\n"
+                    "Versione NT " + ntGet() + " (build SO " + buildGet() + ")\n" + 
+                    (char)169 + " Microsoft Corporation. Tutti i diritti riservati.\n\n" + 
+                    "Il sistema operativo " + OSGet() + " e la relativa interfaccia utente sono protetti da marchi e da altri diritti di propriet\xE0 intelletuale in corso di registrazione o registrati negli Stati Uniti e/o negli altri paesi o aree geografiche";
+    } else {
+    return string("Microsoft ") + OSGet() + " " + commercialVersionGet() + "\n"
+                    "NT Version " + ntGet() + " (OS build " + buildGet() + ")\n" + 
+                    (char)169 + " Microsoft Corporation. All rights reserved.\n\n" + 
+                    "The " + OSGet() + " operating system and its user interface are protected by trademark and other pending or existing intellectual property rights in the United States and other countries/regions";
+    }
+}
+
+string string_5(){
+    if (Language == "0409"){
+        return "Product licensed to:\n" + userGet();
+    } else if (Language == "0410") {
+        return "Prodotto concesso in licenza a:\n" + userGet();
+    } else {
+    return "Product licensed to:\n" + userGet();
+    }
+}
+
+#endif
