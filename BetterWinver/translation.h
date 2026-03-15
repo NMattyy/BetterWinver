ec206b0251622fa2bc04948792b33584a65aabdd
Mattyy <89354292+NMattyy@users.noreply.github.com>
2026-03-15 15:20:52 +0100

    Update translation.h for version 1.4.0

--- BetterWinver/translation.h
+++ BetterWinver/translation.h
@@ -1,65 +1,52 @@
-//BetterWinver 1.3.1
+// BetterWinver 1.4.0
 #ifndef TRANSLATION_H
 #define TRANSLATION_H
 
-#include "infoGet.h"
 #include <string>
 
-using namespace std;
+#include "infoGet.h"
 
-wstring ToWString(const string& s) {
-    if (s.empty()) return wstring();
-    int size_needed = MultiByteToWideChar(CP_UTF8, 0, &s[0], (int)s.size(), NULL, 0);
-    wstring wstrTo(size_needed, 0);
-    MultiByteToWideChar(CP_UTF8, 0, &s[0], (int)s.size(), &wstrTo[0], size_needed);
-    return wstrTo;
+inline std::wstring GetLang() { 
+    static std::wstring Language = currentLanguage(); 
+    return Language; 
+}
+// L"0409" English_US
+// L"0410" Italian
+
+inline std::wstring string_1() {
+    if (GetLang() == L"0410"){ 
+        return L"BetterWinver attualmente supporta solo Windows 8 e successivi (build 9200 e successive)";
+    }
+    return L"Right now BetterWinver only supports Windows 8 and newer (build 9200 and newer)";
 }
 
-string GetLang() { static string L = currentLanguage(); return L; }
-//0409 English_US
-//0410 Italian
-
-string string_1(){
-    if (GetLang() == "0410") return "BetterWinver attualmente supporta solo Windows 8 e successivi (build 9200 e successive)";
-    return "Right now BetterWinver only supports Windows 8 and newer (build 9200 and newer)";
+inline std::wstring string_2() {
+    if (GetLang() == L"0410"){ 
+        return L"Attenzione: versione di Windows non compatibile";
+    }
+    return L"Warning: this version of Windows is not compatible";
 }
 
-string string_2(){
-    if (GetLang() == "0410") return "Attenzione: versione di Windows non compatibile";
-    return "Warning: this version of Windows is not compatible";
+inline std::wstring string_3() {
+    if (GetLang() == L"0410"){ 
+        return L"Informazioni su Windows";
+    }
+    return L"About Windows";
 }
 
-string string_3(){
-    if (GetLang() == "0410") return "Informazioni su Windows";
-    return "About Windows";
-}
-
-wstring string_4(){
-    wstring wOSName = ToWString(OSName);
-    wstring wCommVer = ToWString(commercialVersion);
-    wstring wNT = ToWString(NT);
-    wstring wBuild = ToWString(build);
-
-    if (GetLang() == "0410") {
-        return L"Microsoft " + wOSName + L" " + wCommVer + L"\n"
-               L"Versione NT " + wNT + L" (build SO " + wBuild + L")\n" + 
+inline std::wstring string_4() {
+    if (GetLang() == L"0410") {
+        return L"Microsoft " + OSName + L" " + commercialVersion + L"\n"
+               L"Versione NT " + NT + L" (build SO " + build + L")\n" + 
                wchar_t(169) + L" Microsoft Corporation. Tutti i diritti riservati.\n\n"
-               L"Il sistema operativo " + wOSName + L" e la relativa interfaccia utente sono protetti da marchi e da altri diritti di propriet\x00E0 intellettuale in corso di registrazione o registrati negli Stati Uniti e/o negli altri paesi o aree geografiche";
-    } else {
-        return L"Microsoft " + wOSName + L" " + wCommVer + L"\n"
-               L"NT Version " + wNT + L" (OS build " + wBuild + L")\n" + 
-               wchar_t(169) + L" Microsoft Corporation. All rights reserved.\n\n"
-               L"The " + wOSName + L" operating system and its user interface are protected by trademark and other pending or existing intellectual property rights in the United States and other countries/regions";
+               L"Il sistema operativo " + OSName + L" e la relativa interfaccia utente sono protetti da marchi e da altri diritti di proprietà intellettuale in corso di registrazione o registrati negli Stati Uniti e/o negli altri paesi o aree geografiche\n\n\n\n"
+               L"Prodotto concesso in licenza a:\n" + user;
     }
-}
-
-wstring string_5(){
-    wstring wUser = ToWString(user);
-    if (GetLang() == "0410") {
-        return L"Prodotto concesso in licenza a:\n" + wUser;
-    } else {
-        return L"Product licensed to:\n" + wUser;
-    }
+    return L"Microsoft " + OSName + L" " + commercialVersion + L"\n"
+            L"NT Version " + NT + L" (OS build " + build + L")\n" + 
+            wchar_t(169) + L" Microsoft Corporation. All rights reserved.\n\n"
+            L"The " + OSName + L" operating system and its user interface are protected by trademark and other pending or existing intellectual property rights in the United States and other countries/regions\n\n\n\n"
+            L"Product licensed to:\n" + user;               
 }
 
 #endif
