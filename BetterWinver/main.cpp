4a8260724f179db9a1bf517f07136509eed7e1ec
Mattyy <89354292+NMattyy@users.noreply.github.com>
2026-03-28 20:47:15 +0100

    Release 1.5.0

--- BetterWinver/main.cpp
+++ BetterWinver/main.cpp
@@ -1,7 +1,6 @@
-//BetterWinver 1.4.1
+//BetterWinver 1.5.0
 #include <windows.h>
 #include <dwmapi.h>
-#include <string>
 #include <gdiplus.h>
 #include <wingdi.h>
 #include <shlwapi.h>
@@ -11,31 +10,42 @@
 #include "translation.h"
 
 using namespace Gdiplus;
-using namespace std;
 
 HFONT hFont, hFontBold;
-
-wstring build;
-wstring OSName;
-wstring NT;
-wstring commercialVersion;
-wstring user;
-int compCheck;
-bool isDarkModeEnabled;
-
 COLORREF coloreSfondo = RGB(255, 255, 255);
 COLORREF coloreTesto = RGB(0, 0, 0);
 HBRUSH hBrushSfondo = NULL;
-bool mouseHiglight = false;
+
+wchar_t NT[64];
+wchar_t build[64];
+wchar_t OSName[128];
+wchar_t commercialVersion[64];
+wchar_t user[128];
+int compCheck;
+bool isDarkModeEnabled;
+
 Image* imgLogo = NULL;
 HBITMAP hBmpRes = NULL;
 int bmpWidth = 0, bmpHeight = 0;
+ImageAttributes* pImgAttr = nullptr;
+Bitmap* cachedLogo = nullptr;
+
 FontFamily* ffSegoeV = nullptr;
 Font* gdiFont = nullptr;
 SolidBrush* pTextBrush = nullptr;
+
 Pen* pLinePen = nullptr;
-ImageAttributes* pImgAttr = nullptr;
-Bitmap* cachedLogo = nullptr;
+
+CustomButton btn = {};
+GraphicsPath* buttonPath = nullptr;
+float g_startX, g_startY, g_realW, g_lineY;
+RectF g_layoutRectBody;
+StringFormat* g_pStringFormatCenter = nullptr;
+SolidBrush* pBtnFillNormal = nullptr;
+SolidBrush* pBtnFillHover = nullptr;
+SolidBrush* pBtnFillPressed = nullptr;
+SolidBrush* pBtnTextBrushStatic = nullptr;
+Pen* pBtnBorderPenStatic = nullptr;
 
 LRESULT CALLBACK windowManager(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
     int resourceNumber;
@@ -47,18 +57,32 @@ LRESULT CALLBACK windowManager(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
 
     switch (msg) {
         case WM_CREATE: {
-            UpdateTheme(hwnd, isDarkModeEnabled, coloreSfondo, coloreTesto, hBrushSfondo, gdiFont, ffSegoeV);
+            HMENU hSysMenu = GetSystemMenu(hwnd, FALSE);
+            if (hSysMenu) {
+                RemoveMenu(hSysMenu, SC_SIZE, MF_BYCOMMAND);
+                RemoveMenu(hSysMenu, SC_MAXIMIZE, MF_BYCOMMAND);
+            }
+
+            UpdateTheme(hwnd, isDarkModeEnabled, coloreSfondo, coloreTesto, hBrushSfondo, gdiFont, ffSegoeV, btn);
             logoCreation(resourceNumber);
+
             InvalidateRect(hwnd, NULL, TRUE);   
             return 0;
         }
 
         case WM_PAINT: {
-            UpdateTheme(hwnd, isDarkModeEnabled, coloreSfondo, coloreTesto, hBrushSfondo, gdiFont, ffSegoeV);
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hwnd, &ps);
-            Graphics graphics(hdc);
-            
+
+            HDC memDC = CreateCompatibleDC(hdc);
+            RECT clientRect;
+            GetClientRect(hwnd, &clientRect);
+            int width = clientRect.right;
+            int height = clientRect.bottom;
+            HBITMAP memBitmap = CreateCompatibleBitmap(hdc, width, height);
+            HGDIOBJ oldBitmap = SelectObject(memDC, memBitmap);
+
+            Graphics graphics(memDC);
             graphics.SetInterpolationMode(InterpolationModeHighQualityBicubic);
             graphics.SetSmoothingMode(SmoothingModeHighQuality);
             graphics.SetPixelOffsetMode(PixelOffsetModeHalf);
@@ -66,33 +90,41 @@ LRESULT CALLBACK windowManager(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
                 TextRenderingHintClearTypeGridFit : TextRenderingHintAntiAliasGridFit);
 
             if (compCheck < 22000) {
-                Color backColor;
-                backColor.SetFromCOLORREF(coloreSfondo);
-                graphics.Clear(backColor);
+                Color bkg; bkg.SetFromCOLORREF(coloreSfondo);
+                graphics.Clear(bkg);
             } else {
                 graphics.Clear(Color(0, 0, 0, 0));
             }
 
-            UINT dpi = GetDpiForWindow(hwnd);
-            float startX = ScaleValueF(30.0f, dpi);
-            float startY = ScaleValueF(5.0f, dpi);
-            float realW = (float)ps.rcPaint.right - (startX * 2);
-
             if (imgLogo && imgLogo->GetLastStatus() == Ok) {
-                float aspectHeight = (realW / (float)imgLogo->GetWidth()) * (float)imgLogo->GetHeight();
-                graphics.DrawImage(imgLogo, RectF(startX, startY, realW, aspectHeight));
+                float aspectHeight = (g_realW / (float)imgLogo->GetWidth()) * (float)imgLogo->GetHeight();
+                graphics.DrawImage(imgLogo, RectF(g_startX, g_startY, g_realW, aspectHeight));
             } 
             else if (cachedLogo && cachedLogo->GetLastStatus() == Ok) {
-                float aspectHeight = (realW / (float)cachedLogo->GetWidth()) * (float)cachedLogo->GetHeight();
-                graphics.DrawImage(cachedLogo, RectF(startX, startY, realW, aspectHeight), 0.0f, 0.0f, (REAL)cachedLogo->GetWidth(), (REAL)cachedLogo->GetHeight(), UnitPixel, pImgAttr);
+                float aspectHeight = (g_realW / (float)cachedLogo->GetWidth()) * (float)cachedLogo->GetHeight();
+                graphics.DrawImage(cachedLogo, RectF(g_startX, g_startY, g_realW, aspectHeight), 0.0f, 0.0f, (REAL)cachedLogo->GetWidth(), (REAL)cachedLogo->GetHeight(), UnitPixel, pImgAttr);
             }
 
-            float lineY = ScaleValueF(75.0f, dpi);
-            graphics.DrawLine(pLinePen, startX, lineY, (REAL)ps.rcPaint.right - startX, lineY);
+            graphics.DrawLine(pLinePen, g_startX, g_lineY, (REAL)width - g_startX, g_lineY);
 
-            RectF layoutRectBody(startX, ScaleValueF(90.0f, dpi), realW, ScaleValueF(210.0f, dpi)); 
-            graphics.DrawString(string_4().c_str(), -1, gdiFont, layoutRectBody, NULL, pTextBrush);
+            graphics.DrawString(string_4(), -1, gdiFont, g_layoutRectBody, NULL, pTextBrush);
 
+            SolidBrush* currentFill = pBtnFillNormal;
+            if (btn.pressed) currentFill = pBtnFillPressed;
+            else if (btn.hover) currentFill = pBtnFillHover;
+
+            graphics.FillPath(currentFill, buttonPath);
+            graphics.DrawPath(pBtnBorderPenStatic, buttonPath);
+
+            RectF rect((REAL)btn.rect.left, (REAL)btn.rect.top, (REAL)(btn.rect.right - btn.rect.left), (REAL)(btn.rect.bottom - btn.rect.top));
+
+            graphics.DrawString(L"OK", -1, gdiFont, rect, g_pStringFormatCenter, pBtnTextBrushStatic);
+
+            BitBlt(hdc, 0, 0, width, height, memDC, 0, 0, SRCCOPY);
+
+            SelectObject(memDC, oldBitmap);
+            DeleteObject(memBitmap);
+            DeleteDC(memDC);
             EndPaint(hwnd, &ps);
             return 0;
         }
@@ -106,106 +138,58 @@ LRESULT CALLBACK windowManager(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
             SetBkMode(hdcStatic, TRANSPARENT);
             SetTextColor(hdcStatic, coloreTesto);
             return (LRESULT)hBrushSfondo;
-        }
-        
-        case WM_DRAWITEM: {
-            LPDRAWITEMSTRUCT pDIS = (LPDRAWITEMSTRUCT)lp;
-            if (pDIS->CtlID == 1) { 
-                Graphics graphics(pDIS->hDC);                
-                graphics.SetSmoothingMode(SmoothingModeHighQuality);
-                graphics.SetPixelOffsetMode(PixelOffsetModeHighQuality);
-
-                Color winBackColor(isDarkModeEnabled ? Color(32, 32, 32) : Color(255, 255, 255));
-                SolidBrush clearBrush(winBackColor);
-                graphics.FillRectangle(&clearBrush, 0, 0, pDIS->rcItem.right, pDIS->rcItem.bottom);
-
-                Color btnColor, borderColor, textColor;
-                bool isHovered = mouseHiglight;
-                bool isPressed = (pDIS->itemState & ODS_SELECTED);
-
-                if (isDarkModeEnabled) {
-                    graphics.SetTextRenderingHint(TextRenderingHintClearTypeGridFit);
-                    if (isPressed) btnColor = Color(100, 100, 100, 100);    
-                    else if (isHovered) btnColor = Color(60, 255, 255, 255);                    
-                    else btnColor = Color(30, 255, 255, 255);
-                    
-                    borderColor = Color(100, 100, 100);
-                    textColor = Color(255, 255, 255);
-                } else {
-                    graphics.SetTextRenderingHint(TextRenderingHintAntiAliasGridFit);
-                    if (isPressed) btnColor = Color(100, 0, 0, 0);
-                    else if (isHovered) btnColor = Color(40, 0, 0, 0);
-                    else btnColor = Color(20, 0, 0, 0);
-                    
-                    borderColor = Color(30, 0, 0, 0);
-                    textColor = Color(0, 0, 0);
-                }
-
-                float r = 8.0f;
-                float w = (float)pDIS->rcItem.right - 1.5f;
-                float h = (float)pDIS->rcItem.bottom - 1.5f;
-                float offset = 0.5f;
-
-                GraphicsPath path;
-                path.AddArc(offset, offset, r, r, 180, 90);
-                path.AddArc(w - r, offset, r, r, 270, 90);
-                path.AddArc(w - r, h - r, r, r, 0, 90);
-                path.AddArc(offset, h - r, r, r, 90, 90);
-                path.CloseFigure();
-
-                SolidBrush brush(btnColor);
-                Pen pen(borderColor, 1.0f);
-                
-                graphics.FillPath(&brush, &path);
-                graphics.DrawPath(&pen, &path);
-
-                StringFormat format;
-                format.SetAlignment(StringAlignmentCenter);
-                format.SetLineAlignment(StringAlignmentCenter);
-                SolidBrush textBrush(textColor);
-
-                UINT dpi = GetDpiForWindow(hwnd);
-                const wchar_t* fontName = L"Segoe UI Variable Display";
-                    if (FontFamily(fontName).GetLastStatus() != Ok) {
-                        fontName = L"Segoe UI";
-                    }
-
-                    float scaledBtnFontSize = (9.0f * (float)dpi) / 72.0f;
-                    FontFamily fontFamily(fontName);
-                    Font btnFont(&fontFamily, scaledBtnFontSize, FontStyleRegular, UnitPixel);
-                
-                RectF rectText(0, 0, (REAL)pDIS->rcItem.right, (REAL)pDIS->rcItem.bottom);
-                graphics.DrawString(L"OK", -1, &btnFont, rectText, &format, &textBrush);
-
-                return TRUE;
-            }
-            break;
-        }
+        }        
 
         case WM_SETCURSOR: {
-            if ((HWND)wp == GetDlgItem(hwnd, 1)) {
-                if (!mouseHiglight) {
-                    mouseHiglight = true;
-                    InvalidateRect((HWND)wp, NULL, FALSE);
-                }
-                SetCursor(LoadCursor(NULL, IDC_ARROW));
-                return TRUE; 
-            } 
-            else {
-                if (mouseHiglight) {
-                    mouseHiglight = false;
-                    InvalidateRect(GetDlgItem(hwnd, 1), NULL, FALSE);
-                }
-                SetCursor(LoadCursor(NULL, IDC_ARROW));
-                return TRUE;
-            }
+            SetCursor(LoadCursor(NULL, IDC_ARROW));
+            return TRUE; 
+        }
+        
+        case WM_MOUSEMOVE: {
+            POINT pt = { LOWORD(lp), HIWORD(lp) };
+
+            bool old = btn.hover;
+            btn.hover = PtInRect(&btn.rect, pt);
+
+            if (old != btn.hover)
+                InvalidateRect(hwnd, &btn.rect, FALSE);
+
+            TRACKMOUSEEVENT tme = { sizeof(tme), TME_LEAVE, hwnd, 0 };
+            TrackMouseEvent(&tme);
+
+            return 0;
         }
 
-        case WM_COMMAND: {
-            if (LOWORD(wp) == 1) {
-                SendMessage(hwnd, WM_CLOSE, 0, 0);
+        case WM_MOUSELEAVE: {
+            btn.hover = false;
+            InvalidateRect(hwnd, &btn.rect, FALSE);
+            return 0;
+        }
+
+        case WM_LBUTTONDOWN: {
+            POINT pt = { LOWORD(lp), HIWORD(lp) };
+
+            if (PtInRect(&btn.rect, pt)) {
+                btn.pressed = true;
+                SetCapture(hwnd);
+                InvalidateRect(hwnd, &btn.rect, FALSE);
             }
-            break;
+            return 0;
+        }
+
+        case WM_LBUTTONUP: {
+            POINT pt = { LOWORD(lp), HIWORD(lp) };
+
+            if (btn.pressed) {
+                btn.pressed = false;
+                ReleaseCapture();
+
+                if (PtInRect(&btn.rect, pt))
+                    SendMessage(hwnd, WM_CLOSE, 0, 0);
+
+                InvalidateRect(hwnd, &btn.rect, FALSE);
+            }
+            return 0;
         }
 
         case WM_KEYDOWN: {
@@ -214,31 +198,24 @@ LRESULT CALLBACK windowManager(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
             }
             return 0;
         }
-        
+
         case WM_SETTINGCHANGE: {
-            UpdateTheme(hwnd, isDarkModeEnabled, coloreSfondo, coloreTesto, hBrushSfondo, gdiFont, ffSegoeV);
+            UpdateTheme(hwnd, isDarkModeEnabled, coloreSfondo, coloreTesto, hBrushSfondo, gdiFont, ffSegoeV, btn);
+            InvalidateRect(hwnd, NULL, TRUE);
+            UpdateWindow(hwnd);
             return 0;
         }
 
         case WM_DPICHANGED: {
             LPRECT lprcNewScale = (LPRECT)lp;
             UINT newDpi = LOWORD(wp);
-            UpdateTheme(hwnd, isDarkModeEnabled, coloreSfondo, coloreTesto, hBrushSfondo, gdiFont, ffSegoeV);
+            UpdateTheme(hwnd, isDarkModeEnabled, coloreSfondo, coloreTesto, hBrushSfondo, gdiFont, ffSegoeV, btn);
             logoCreation(resourceNumber);
 
             SetWindowPos(hwnd, NULL, lprcNewScale->left, lprcNewScale->top, lprcNewScale->right - lprcNewScale->left, lprcNewScale->bottom - lprcNewScale->top, SWP_NOZORDER | SWP_NOACTIVATE);
 
             if (gdiFont) { delete gdiFont; gdiFont = nullptr; }
             if (ffSegoeV) { delete ffSegoeV; ffSegoeV = nullptr; }
-
-            HWND hBtn = GetDlgItem(hwnd, 1);
-            int btnW = ScaleValue(68, newDpi);
-            int btnH = ScaleValue(22, newDpi);
-            RECT clientRect;
-            GetClientRect(hwnd, &clientRect);
-            int btnX = clientRect.right - btnW - ScaleValue(30, newDpi);
-            int btnY = clientRect.bottom - btnH - ScaleValue(15, newDpi);
-            MoveWindow(hBtn, btnX, btnY, btnW, btnH, TRUE);
 
             InvalidateRect(hwnd, NULL, TRUE);
             UpdateWindow(hwnd);
@@ -256,6 +233,13 @@ LRESULT CALLBACK windowManager(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
             if (pLinePen) delete pLinePen;
             if (pImgAttr) delete pImgAttr;
             if (cachedLogo) delete cachedLogo;
+            if (buttonPath) delete buttonPath;
+            if (g_pStringFormatCenter) delete g_pStringFormatCenter;
+            if (pBtnFillNormal) delete pBtnFillNormal;
+            if (pBtnFillHover) delete pBtnFillHover;
+            if (pBtnFillPressed) delete pBtnFillPressed;
+            if (pBtnTextBrushStatic) delete pBtnTextBrushStatic;
+            if (pBtnBorderPenStatic) delete pBtnBorderPenStatic;
             
             PostQuitMessage(0);
             break;
@@ -271,10 +255,11 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmd, int nShow) {
     icce.dwICC = ICC_STANDARD_CLASSES;
     InitCommonControlsEx(&icce);
     
-    build = buildGet();
-    compCheck = stoi(build);
+    buildGet(build, 64);
+    compCheck = _wtoi(build);
+
     if (compCheck < 9200) {
-        MessageBoxW(NULL, string_1().c_str(), string_2().c_str(), MB_OK | MB_ICONWARNING | MB_SETFOREGROUND);
+        MessageBoxW(NULL, string_1(), string_2(), MB_OK | MB_ICONWARNING | MB_SETFOREGROUND);
         return 0;
     }
 
@@ -287,10 +272,10 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmd, int nShow) {
     int windowW = rc.right - rc.left;
     int windowH = rc.bottom - rc.top;
 
-    OSName = OSGet();
-    NT = ntGet();
-    commercialVersion = commercialVersionGet();
-    user = userGet();
+    OSGet(OSName, 128);
+    ntGet(NT, 64);
+    commercialVersionGet(commercialVersion, 64);
+    userGet(user, 128);
     DarkModeCheck();
 
     GdiplusStartupInput gdiplusStartupInput;
@@ -304,23 +289,9 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmd, int nShow) {
     wc.lpszClassName = L"BetterWinver";
     RegisterClassW(&wc);
 
-    HWND hwnd = CreateWindowW(L"BetterWinver", string_3().c_str(), WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, windowW, windowH, NULL, NULL, hInst, NULL);
+    HWND hwnd = CreateWindowExW(WS_EX_DLGMODALFRAME, L"BetterWinver", string_3(), WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, windowW, windowH, NULL, NULL, hInst, NULL);
 
-    int btnW = ScaleValue(68, currentDpi);
-    int btnH = ScaleValue(22, currentDpi);
-    int btnX = clientW - btnW - ScaleValue(30, currentDpi); 
-    int btnY = clientH - btnH - ScaleValue(15, currentDpi);
-
-    if (compCheck >= 22000) {
-        int backdropType = DWMSBT_MAINWINDOW;
-        DwmSetWindowAttribute(hwnd, DWMWA_SYSTEMBACKDROP_TYPE, &backdropType, sizeof(backdropType));
-        MARGINS margins = {-1, -1, -1, -1};
-        DwmExtendFrameIntoClientArea(hwnd, &margins);
-    }
-
-    HWND hButton = CreateWindowW(L"BUTTON", L"OK", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_OWNERDRAW, btnX, btnY, btnW, btnH, hwnd, (HMENU)1, hInst, NULL);
-    SendMessage(hButton, WM_SETFONT, (WPARAM)hFont, TRUE);
-
+    windowTheme(hwnd);
     ShowWindow(hwnd, nShow);
     UpdateWindow(hwnd);
 
